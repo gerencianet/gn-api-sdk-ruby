c622e8954668573072c117768bc106caa9bfd4b4
Francisco <f.thiene@gmail.com>
2015-11-16 14:58:57 -0200

    Update credentials.rb

--- docs/examples/credentials.rb
+++ docs/examples/credentials.rb
@@ -1,5 +1,5 @@
 # example
 module CREDENTIALS
-  CLIENT_ID = "Client_Id_87927ea664846b546adb3708b2ad16ed3c00f02b"
-  CLIENT_SECRET = "Client_Secret_d8da11a01fe157db2bc35e4e6828cccf092db9e2"
+  CLIENT_ID = ""
+  CLIENT_SECRET = ""
 end
