d6b3d7cfd24f039d3f8322029f82b223fc14c5df
jessica-gavaGN <75253668+jessica-gavaGN@users.noreply.github.com>
2021-07-01 11:11:17 -0300

    Added: API Pix endpoints
    Update Qrcode image path in pix_generate_qrcode.rb

--- examples/pix/location/pix_generate_qrcode.rb
+++ examples/pix/location/pix_generate_qrcode.rb
@@ -22,5 +22,5 @@ puts response['qrcode']
 
 data = response['imagemQrcode']
 image_data = Base64.decode64(data['data:image/png;base64,'.length .. -1])
-new_file=File.new("./../../../imgQrcode/qrCodeImage.png", 'wb') #salva a imagem do qrcode na pasta imgQrcode no diretório raiz
+new_file=File.new("../../../qrCodeImage.png", 'wb') #salva a imagem do qrcode no diretório raiz
 new_file.write(image_data)
